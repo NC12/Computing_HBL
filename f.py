8eb0ea3a1338359744e31c416a4b7df3ec52db34
Ng Cheryl <ng.cheryl@dhs.sg>
2013-12-30 16:55:01 +0800

    f edited

--- f.py
+++ f.py
@@ -1,16 +1,16 @@
 def same_score(players):
     same = []
-    occurrence = 0
-    for i in range(len(players) - 1):
-        score = players[i][1]
-        if score == players[i+1][1] and occurrence == 0:
-            same.append(players[i])
-            same.append(players[i+1])
-            occurrence = 1
-        elif score == players[i+1][1] and occurrence == 1:
-            same.append(players[i+1])
-        else:
-            occurence = 0
+    pos = 0
+    # start with 1st score
+    score = players[0][1]
+    while pos < len(players) - 1:
+        # compare score with next score
+        while score == players[pos+1][1]:
+            same.append(players[pos])
+            pos +=1
+            # if 2nd last score and last score are the same
+            if pos == len(players) - 1:
+                same.append(players[pos])
+        # if score not same with next score, move to the next score to compare
+        score = players[pos][1]
     print(same)
-
-#same_score(players)
